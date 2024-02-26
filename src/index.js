46467f426a3d991b0476e3ec44d2fcca2cc7b7ad
FreeLancerrrrr <asealmohmand@gmail.com>
2024-02-25 18:39:39 -0600

    Updated/tested react router (index.js), landing page displays same as app.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Landing from './front-end/landing';
 
+//need to fix this router, its inefficient. (im pretty sure)
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+
+ 
+);
+
+
+ReactDOM.render(
+<Landing />,
+document.getElementById('landing')
 );
