caff14a52a921f1b75c64ea8498bfdb644486dad
FreeLancerrrrr <asealmohmand@gmail.com>
2024-04-20 20:12:38 -0500

    Generation UI updates

--- src/front-end/generation.js
+++ src/front-end/generation.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import SheetMusicComponent from './SheetMusicComponent';
-import { TextField,  Paper, Grid, CircularProgress, Typography, Button, Select, MenuItem } from '@mui/material';
+import { TextField,  Paper, Grid, CircularProgress, Typography, Button, Select, MenuItem, FormControl, InputLabel, OutlinedInput } from '@mui/material';
 import Header from './components/polypalHeader';
 import Keydropdown from './components/keysigdropdown';
 import css from "./components/frontEnd.module.css"
@@ -82,7 +82,10 @@ const upload = async () => {
 //once music is set, render
 //drop down to be removed!
 
-const circleOfFifths = "C,G,D,A,E,B,C-,F#,G-,C#,D-,A-,E-,B-,F,a,e,b,f#,c#,g#,e-,d#,b-,f,c,g,d".split(',');
+const circleOfFifths = "C,G,D,A,E,B,C-,F#,G-,C#,D-,A-,E-,B-,F,a,e,b,f#,c#,g#,e-,d#,b-,f,c,g,d".split(',');const title ="Generate Music"
+const subtitle ="Input chords/roman numerals and key signature"
+const thirdtitle ="Please enter a comma separated list ex) I,ii,V7,I"
+const arr = {upload_title: title, upload_subtitle: subtitle, upload_thirdtitle: thirdtitle}
 
 const render_content = () =>
 {   
@@ -93,31 +96,52 @@ const render_content = () =>
         if(musicXML){//if data is recieved, render it
             return <>
                 {generating ? (<div><CircularProgress/> <p>Generating Data</p></div>) : (null)}
-                {musicXML.map(xml => <SheetMusicComponent musicXml={xml.xml} key={xml.id} />)}</>;
+                
+                
+                {musicXML.map(xml => 
+                <Grid pb={2} maxWidth={"50vh"}>
+                <Paper className={css.music_paper} >
+                <SheetMusicComponent musicXml={xml.xml} key={xml.id} />
+                </Paper>
+                </Grid>)}
+                
+                </>;
         }
         else{//wait for user input
-            const titleTXT ="Generate Music"
-            const subTXT ="Input chords/roman numerals and key signature"
-            const thirdTXT ="Please enter a comma separated list ex) I,ii,V7,I"
+            
             return(
-                <Grid>
-                <TextField id="outlined-basic" label="Roman Numerals" variant="outlined" value={textVal} onChange={handleTextChange}/>
-                <Select value={ddValue} onChange={handleDDchange}>
-                    {circleOfFifths.map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
-                </Select> 
-                <Button onClick={upload}>Upload</Button>
-                
+                <div>
+                <Grid container mt={{xs:20, sm:20, md:20, lg:20 , xl:20}} className={css.flex_container} >
+                    <Grid item align="center">
+                    <Paper className={css.upload_paper} elevation={3}>
+                        {Object.keys(arr).map(key => <Typography className={css[key]}>{arr[key]}</Typography>)}
+                        {console.log(Object.keys(arr))}
+                        
+                        <TextField id="outlined-basic" label="Roman Numerals" variant="outlined" sx={{width: 200}} value={textVal} onChange={handleTextChange}/>
+                        
+                        <FormControl variant="outlined" sx={{width:175}}>
+                        <InputLabel id="Key">Key Signature</InputLabel>
+                        <Select value={ddValue} onChange={handleDDchange} input={<OutlinedInput label="Key Signature" />}>
+                            {circleOfFifths.map(key => <MenuItem key={key} value={key} >{key}</MenuItem>)}
+                        </Select>
+                        </FormControl> 
+                        <Button className={css.btnLG} onClick={upload}>Upload</Button>
+                    </Paper>  
+                </Grid>
             </Grid>
+            <div className={css.upload_background}></div>
+            </div>
             );
         }
     }
 
 }
     return(
-        <div>
-            <Header/>
+        <div >
+            
+            <Header />
             {render_content()}
-            <div className={css.upload_background}></div>
+            
         </div>
     );
 }
