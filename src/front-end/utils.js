277c8c6ab12b18ac83b5bc409df5aa273486fa4b
Alexander N Chin <anc200008@utdallas.edu>
2024-04-23 02:02:59 -0500

    adapted prod changes for development

--- src/front-end/utils.js
+++ src/front-end/utils.js
@@ -1,1 +1,5 @@
-export const HOST = "https://polyphony-pal-production.up.railway.app"
+// 1. uncomment the below string for prod!!
+// export const HOST = "https://polyphony-pal-production.up.railway.app"
+
+// 2. comment the below string for prod!!
+export const HOST = "http://localhost:5001"
