f0ee468135e033931e37ccee4bdea284ba366b85
Alexander N. Chin <anc200008@utdallas.edu>
2024-04-26 11:19:23 -0500

    update util

--- src/front-end/utils.js
+++ src/front-end/utils.js
@@ -1,5 +1,5 @@
 // 1. uncomment the below string for prod!!
-// export const HOST = "https://polyphony-pal-production.up.railway.app"
+export const HOST = "https://polyphony-pal-production.up.railway.app"
 
 // 2. comment the below string for prod!!
-export const HOST = "http://localhost:5001"
+// export const HOST = "http://localhost:5001"
